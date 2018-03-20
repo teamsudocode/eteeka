d9312d5620466d8c6ea132c7497b9956fc36a0e5
Abhishek Sharma <hello.abhishek@outlook.com>
2018-03-20 17:29:21 +0530

    added hindi support

--- platforms/android/assets/www/js/notification.js
+++ platforms/android/assets/www/js/notification.js
@@ -1,9 +1,19 @@
-"use strict";
-
-document.addEventListener('deviceready', function () {    
-    cordova.plugins.notification.local.schedule({
-        title: 'My first notification',
-        text: 'Thats pretty easy...',
-        foreground: true
-    });
+document.addEventListener('deviceready', function () {
+    
+    
+    document.getElementById('not').addEventListener('click', function () {
+        cordova.plugins.notification.local.schedule({
+            title: 'Notifications are on!',
+            text: "You will recieve a notification when \n your child's vaccine is due.",
+            trigger: { in: 0,
+                unit: 'second'
+            },
+            foreground: true,
+            smallIcon: 'res://icon'
+        });
+        
+        setTimeout(function(){
+            window.location.href="home.html";
+        },1000)
+    })
 }, false);
